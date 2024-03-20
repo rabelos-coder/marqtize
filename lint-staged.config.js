3bae3a037f41242d839758d95d126d9a3fb8890d
Fabricio Pereira Rabelo <fabriciojbo@icloud.com>
2024-03-20 01:14:59 -0300

    feat: removed cypress support defaults

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,7 @@ module.exports = {
   '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
   // Lint then format TypeScript and JavaScript files
-  '**/*.(ts|tsx|js)': (filenames) => [`yarn lint:fix`, `yarn format:fix`],
+  '**/*.(ts|tsx|js)': () => [`yarn lint:fix`, `yarn format:fix`],
 
   // Format MarkDown and JSON
   '**/*.(md|json)': (filenames) =>
