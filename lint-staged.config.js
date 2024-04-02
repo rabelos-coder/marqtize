fbb99a8376362369d863fbf0d93fb4504a050212
Fabricio Pereira Rabelo <fabriciojbo@icloud.com>
2024-04-02 19:25:18 -0300

    feat: updated

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+  './src/**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
   // Lint then format TypeScript and JavaScript files
-  '**/*.(ts|tsx|js)': () => [`yarn lint:fix`, `yarn format:fix`],
+  './src/**/*.(ts|tsx|js)': () => [`yarn lint:fix`, `yarn format:fix`],
 
   // Format MarkDown and JSON
   '**/*.(md|json)': (filenames) =>
