d7ceb39cc15cdbb912aa4b364072e8bed945e243
Fabricio Pereira Rabelo <fabriciojbo@icloud.com>
2024-04-12 12:09:32 -0300

    fix: updated

---
+++ src/app/api/blog/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { FIND_ACCOUNT_BY_SLUG } from '@/graphql/account'
+import { apiClient } from '@/utils/apollo'
+
+export async function POST(req: NextRequest) {
+  const data = await req.json()
+
+  // sanitize data
+  const slug = data?.slug?.trim() ?? null
+  const host = data?.host?.trim() ?? null
+
+  if (!slug || !host) {
+    return NextResponse.json({ message: 'Invalid request' }, { status: 400 })
+  }
+
+  // call the login mutation
+  return await apiClient
+    .mutate({
+      mutation: FIND_ACCOUNT_BY_SLUG,
+      variables: { slug, host },
+      fetchPolicy: 'no-cache',
+    })
+    .then(({ data }) =>
+      NextResponse.json(data?.findBySlugOrHostAccount ?? null)
+    )
+    .catch((error) =>
+      NextResponse.json(
+        {
+          name: error.name,
+          message: error.message,
+          stack: error?.stack ?? null,
+        },
+        { status: 400 }
+      )
+    )
+}
